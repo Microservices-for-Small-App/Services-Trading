cba72a457fe4515da3fc4278bd31685180b2b85c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-23 17:01:30 +0530

    Code Changes

---
+++ src/Trading.API/Contracts/PurchaseRequested.cs
@@ -0,0 +1,8 @@
+﻿namespace Trading.API.Contracts;
+
+public record PurchaseRequested(
+        Guid UserId,
+        Guid ItemId,
+        int Quantity,
+        Guid CorrelationId
+    );
--- src/Trading.API/StateMachines/PurchaseState.cs
+++ src/Trading.API/StateMachines/PurchaseState.cs
@@ -1,6 +1,27 @@
-﻿namespace Trading.API.StateMachines
+﻿using Automatonymous;
+using MassTransit.Saga;
+
+namespace Trading.API.StateMachines;
+
+public class PurchaseState : SagaStateMachineInstance, ISagaVersion
 {
-    public class PurchaseState
-    {
-    }
+    public Guid CorrelationId { get; set; }
+    
+    public string CurrentState { get; set; } = string.Empty;
+
+    public Guid UserId { get; set; }
+    
+    public Guid ItemId { get; set; }
+    
+    public int Quantity { get; set; }
+    
+    public DateTimeOffset Received { get; set; }
+    
+    public decimal? PurchaseTotal { get; set; }
+    
+    public DateTimeOffset LastUpdated { get; set; }
+
+    public string ErrorMessage { get; set; } = string.Empty;
+    
+    public int Version { get; set; }
 }
