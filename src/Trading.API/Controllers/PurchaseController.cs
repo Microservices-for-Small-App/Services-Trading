2338a5515041957d503778c2a349acf9bdb8b3cc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-23 17:27:25 +0530

    Controller

---
+++ src/Trading.API/Controllers/PurchaseController.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Trading.API.Contracts;
+using Trading.API.Dtos;
+
+namespace Trading.API.Controllers;
+
+[Route("api/purchase")]
+[ApiController]
+[Authorize]
+public class PurchaseController : ControllerBase
+{
+    private readonly IPublishEndpoint publishEndpoint;
+
+    public PurchaseController(IPublishEndpoint publishEndpoint)
+    {
+        this.publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(SubmitPurchaseDto purchase)
+    {
+        var userId = User.FindFirstValue("sub");
+        var correlationId = Guid.NewGuid();
+
+        var message = new PurchaseRequested(Guid.Parse(userId!), purchase.ItemId!.Value, purchase.Quantity, correlationId);
+
+        await publishEndpoint.Publish(message);
+
+        return Accepted();
+    }
+}
---
+++ src/Trading.API/Dtos/SubmitPurchaseDto.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trading.API.Dtos;
+
+public record SubmitPurchaseDto([Required] Guid? ItemId, [Range(1, 100)] int Quantity);
