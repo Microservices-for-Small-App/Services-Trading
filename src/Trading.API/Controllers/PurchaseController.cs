63dfcef9a3adaf0cff54a6d977ad3756f49b554b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-24 21:51:41 +0530

    Update PurchaseController.cs

--- src/Trading.API/Controllers/PurchaseController.cs
+++ src/Trading.API/Controllers/PurchaseController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using Trading.API.Contracts;
 using Trading.API.Dtos;
+using Trading.API.StateMachines;
 
 namespace Trading.API.Controllers;
 
@@ -13,10 +14,26 @@ namespace Trading.API.Controllers;
 public class PurchaseController : ControllerBase
 {
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly IRequestClient<GetPurchaseState> _purchaseClient;
 
-    public PurchaseController(IPublishEndpoint publishEndpoint)
+    public PurchaseController(IPublishEndpoint publishEndpoint, IRequestClient<GetPurchaseState> purchaseClient)
     {
         _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+
+        _purchaseClient = purchaseClient ?? throw new ArgumentNullException(nameof(purchaseClient));
+    }
+
+    [HttpGet("status/{correlationId}")]
+    public async Task<ActionResult<PurchaseDto>> GetStatusAsync(Guid correlationId)
+    {
+        var response = await _purchaseClient.GetResponse<PurchaseState>(new GetPurchaseState(correlationId));
+
+        var purchaseState = response.Message;
+
+        var purchase = new PurchaseDto(purchaseState.UserId, purchaseState.ItemId, purchaseState.PurchaseTotal,
+            purchaseState.Quantity, purchaseState.CurrentState!, purchaseState.ErrorMessage!, purchaseState.Received, purchaseState.LastUpdated);
+
+        return Ok(purchase);
     }
 
     [HttpPost]
@@ -29,6 +46,7 @@ public class PurchaseController : ControllerBase
 
         await _publishEndpoint.Publish(message);
 
-        return Accepted();
+        return AcceptedAtAction(nameof(GetStatusAsync), new { correlationId }, new { correlationId });
     }
+
 }
