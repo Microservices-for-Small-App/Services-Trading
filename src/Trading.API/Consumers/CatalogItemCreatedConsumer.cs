88063ea497fb3436a6458b85a5fb7d44e98cae1a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-25 10:19:53 +0530

    Swamy/24mar work (#2)
    * Packages and Configuration
    
    * Configuring Services
    
    * Update Program.cs
    
    * Changes
    
    * Update PurchaseState.cs
    
    * Added New Dto
    
    * Update PurchaseStateMachine.cs
    
    * Create PurchaseDto.cs
    
    * Update PurchaseController.cs
    
    * Update Program.cs
    
    * Update Program.cs
    
    * Create CatalogItem.cs
    
    * Consumers
    
    * Consumers
    
    * Create CatalogItemUpdatedConsumer.cs
    
    * Update Program.cs

---
+++ src/Trading.API/Consumers/CatalogItemCreatedConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.Contracts;
+using CommonLibrary.Interfaces;
+using MassTransit;
+using Trading.API.Entities;
+
+namespace Trading.API.Consumers;
+
+public class CatalogItemCreatedConsumer : IConsumer<CatalogItemCreated>
+{
+    private readonly IRepository<CatalogItem> _repository;
+
+    public CatalogItemCreatedConsumer(IRepository<CatalogItem> repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public async Task Consume(ConsumeContext<CatalogItemCreated> context)
+    {
+        var message = context.Message;
+
+        var item = await _repository.GetAsync(message.ItemId);
+
+        if (item is not null)
+        {
+            return;
+        }
+
+        item = new CatalogItem
+        {
+            Id = message.ItemId,
+            Name = message.Name,
+            Description = message.Description,
+            Price = message.Price
+        };
+
+        await _repository.CreateAsync(item);
+    }
+}
--- src/Trading.API/Program.cs
+++ src/Trading.API/Program.cs
@@ -1,8 +1,27 @@
+using CommonLibrary.Identity;
+using CommonLibrary.MassTransit;
+using CommonLibrary.MongoDB.Extensions;
+using CommonLibrary.Settings;
+using MassTransit;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Trading.API.Entities;
+using Trading.API.StateMachines;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMongo()
+                .AddMongoRepository<CatalogItem>("catalogitems")
+                .AddJwtBearerAuthentication();
 
-builder.Services.AddControllers();
+AddMassTransit(builder.Services);
+
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+}).AddJsonOptions(options => options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,3 +42,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddMassTransit(IServiceCollection services)
+{
+    _ = services.AddMassTransit(configure =>
+    {
+        configure.UseRabbitMqService();
+
+        configure.AddConsumers(Assembly.GetEntryAssembly());
+
+        _ = configure.AddSagaStateMachine<PurchaseStateMachine, PurchaseState>()
+            .MongoDbRepository(r =>
+            {
+                var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                var mongoSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
+                r.Connection = mongoSettings!.ConnectionString;
+                r.DatabaseName = serviceSettings!.ServiceName;
+            });
+    });
+
+    _ = services.AddMassTransitHostedService();
+
+    _ = services.AddGenericRequestClient();
+}
