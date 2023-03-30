ca750fc10f219ab17146f1d474baba1f68a3b1c0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-30 22:39:59 +0530

    New Entities

---
+++ src/Trading.API/Entities/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿using CommonLibrary.Entities;
+
+namespace Trading.API.Entities;
+
+public class InventoryItem : IEntity
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public Guid CatalogItemId { get; set; }
+
+    public int Quantity { get; set; }
+}
