be75ce266caeb79b2800d6fd96bc310c5fa640c0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-30 23:08:29 +0530

    New version of contracts

---
+++ src/Trading.API/Consumers/UserUpdatedConsumer.cs
@@ -0,0 +1,40 @@
+﻿using CommonLibrary.Interfaces;
+using Identity.Contracts;
+using MassTransit;
+using Trading.API.Entities;
+
+namespace Trading.API.Consumers;
+
+public class UserUpdatedConsumer : IConsumer<UserUpdated>
+{
+    private readonly IRepository<ApplicationUser> repository;
+
+    public UserUpdatedConsumer(IRepository<ApplicationUser> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task Consume(ConsumeContext<UserUpdated> context)
+    {
+        var message = context.Message;
+
+        var user = await repository.GetAsync(message.UserId);
+
+        if (user == null)
+        {
+            user = new ApplicationUser
+            {
+                Id = message.UserId,
+                Gil = message.NewTotalGil
+            };
+
+            await repository.CreateAsync(user);
+        }
+        else
+        {
+            user.Gil = message.NewTotalGil;
+
+            await repository.UpdateAsync(user);
+        }
+    }
+}
