8a387a65f595d5c52c4e43bbbc69af9f8b988081
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-31 15:26:57 +0530

    Create StoreController.cs

---
+++ src/Trading.API/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using CommonLibrary.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trading.API.Entities;
+
+namespace Trading.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class StoreController : ControllerBase
+{
+    private readonly IRepository<CatalogItem> _catalogRepository;
+    private readonly IRepository<ApplicationUser> _usersRepository;
+    private readonly IRepository<InventoryItem> _inventoryRepository;
+
+    public StoreController(IRepository<CatalogItem> catalogRepository, IRepository<ApplicationUser> usersRepository, IRepository<InventoryItem> inventoryRepository)
+    {
+        _catalogRepository = catalogRepository ?? throw new ArgumentNullException(nameof(catalogRepository));
+
+        _usersRepository = usersRepository ?? throw new ArgumentNullException(nameof(usersRepository));
+
+        _inventoryRepository = inventoryRepository ?? throw new ArgumentNullException(nameof(inventoryRepository));
+    }
+
+
+}
