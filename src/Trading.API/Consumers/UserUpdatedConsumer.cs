0f8bfe460b98a0af2bb3ce640247699d5840d19b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-31 13:28:04 +0530

    Swamy/30mar work (#8)
    * Update Trading.API.csproj
    
    * New Entities
    
    * New Consumer
    
    * New version of contracts
    
    * Code Refactoring

---
+++ src/Trading.API/Consumers/UserUpdatedConsumer.cs
@@ -0,0 +1,40 @@
+﻿using CommonLibrary.Interfaces;
+using Identity.Contracts;
+using MassTransit;
+using Trading.API.Entities;
+
+namespace Trading.API.Consumers;
+
+public class UserUpdatedConsumer : IConsumer<UserUpdated>
+{
+    private readonly IRepository<ApplicationUser> _repository;
+
+    public UserUpdatedConsumer(IRepository<ApplicationUser> repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public async Task Consume(ConsumeContext<UserUpdated> context)
+    {
+        var message = context.Message;
+
+        var user = await _repository.GetAsync(message.UserId);
+
+        if (user is null)
+        {
+            user = new ApplicationUser
+            {
+                Id = message.UserId,
+                Gil = message.NewTotalGil
+            };
+
+            await _repository.CreateAsync(user);
+        }
+        else
+        {
+            user.Gil = message.NewTotalGil;
+
+            await _repository.UpdateAsync(user);
+        }
+    }
+}
---
+++ src/Trading.API/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using CommonLibrary.Entities;
+
+namespace Trading.API.Entities;
+
+public class ApplicationUser : IEntity
+{
+    public Guid Id { get; set; }
+
+    public decimal Gil { get; set; }
+}
