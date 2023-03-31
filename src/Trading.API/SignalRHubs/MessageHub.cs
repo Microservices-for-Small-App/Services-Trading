d4344ed930fdeeb42c2d63ef8854317ae2a0176d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-31 22:32:32 +0530

    SignalR Hub

---
+++ src/Trading.API/SignalRHubs/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Trading.API.StateMachines;
+
+namespace Trading.API.SignalRHubs;
+
+[Authorize]
+public class MessageHub : Hub
+{
+    public async Task SendStatusAsync(PurchaseState status)
+    {
+        if (Clients is not null)
+        {
+            await Clients.User(Context.UserIdentifier!).SendAsync("ReceivePurchaseStatus", status);
+        }
+    }
+}
